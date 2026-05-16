0b722251481995553aaf9cc6c6f23bd97f5ebba1
Xileff <felixsavero78@gmail.com>
2026-05-16 12:32:16 +0700

    feat: add variadic function example

--- func.go
+++ func.go
@@ -6,12 +6,20 @@ func main() {
 	helloFelix := getHello("Felix")
 	fmt.Println(helloFelix)
 
+	// multiple return value
 	_, lastName := getFullName()
 	fmt.Println(lastName)
 
 	firstName, lastName := getNamedFullName()
 	fmt.Println(firstName)
 	fmt.Println(lastName)
+
+	// variadic function (varargs)
+	fmt.Println(sumAll(1, 2, 3, 4, 5, 6))
+
+	numSlice := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(sumAll(numSlice...)) // like spred operator in TS
+
 }
 
 func getHello(name string) string {
@@ -27,4 +35,14 @@ func getNamedFullName() (firstName string, lastName string) {
 	lastName = "Savero"
 
 	return firstName, lastName
+}
+
+func sumAll(numbers ...int) int {
+	sum := 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
 }
