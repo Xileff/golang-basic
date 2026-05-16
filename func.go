47e9fa2da3f09c4c69bc82f1a2ef88dc59576f1f
Xileff <felixsavero78@gmail.com>
2026-05-16 12:18:52 +0700

    feat: create function with multiple return value

--- func.go
+++ func.go
@@ -5,8 +5,15 @@ import "fmt"
 func main() {
 	helloFelix := getHello("Felix")
 	fmt.Println(helloFelix)
+
+	_, lastName := getFullName()
+	fmt.Println(lastName)
 }
 
 func getHello(name string) string {
 	return "Hello " + name
+}
+
+func getFullName() (string, string) {
+	return "Full", "Name"
 }
