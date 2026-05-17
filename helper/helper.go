f5a48b39ec7c0ef896bdef8f022cfcef5010a56c
Xileff <felixsavero78@gmail.com>
2026-05-17 18:20:58 +0700

    feat: add private function example in helper

--- helper/helper.go
+++ helper/helper.go
@@ -2,6 +2,14 @@ package helper
 
 import "fmt"
 
+// private
+var version = "1.0.0"
+
+func sayGoodbye() {
+	fmt.Println("Goodbye")
+}
+
+// public
 func SayHello(name string) {
 	fmt.Println("Hello ", name)
 }
